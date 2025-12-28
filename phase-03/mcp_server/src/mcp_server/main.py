df9f37c5445189b262635af7e521a14eaa61226c
Syeda Hoorain Ali <syedahoorainali8@gmail.com>
2025-12-28 16:02:02 +0500

    Update mcp_server/main.py

--- phase-03/mcp_server/src/mcp_server/main.py
+++ phase-03/mcp_server/src/mcp_server/main.py
@@ -162,12 +162,12 @@ async def update_task(request: UpdateTaskRequest) -> UpdateTaskResponse:
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_db()
-    # mcp.run(transport="streamable-http")
+    mcp.run(transport="streamable-http")
     yield
     
 
 app = FastAPI(name="todo-mcp-server", lifespan=lifespan)
-app.mount("/mcp", mcp.streamable_http_app())
+# app.mount("/mcp", mcp.streamable_http_app())
 
 if __name__ == "__main__":
     import sys
